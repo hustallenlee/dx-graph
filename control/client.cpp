f20bad165b71e5eb7e49b10513782db7551d4f13
allen lee <allen_lee922@foxmail.com>
2015-12-12 06:06:01 +0800

    change something

--- control/client.cpp
+++ control/client.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char * argv[]){
 	controller_client client("./machine.ini");
 	int type;
+	int i =0;
 	while(1){
 		type = client.recv_msg();
 		int flag = 0;
@@ -28,8 +29,9 @@ int main(int argc, char * argv[]){
 		if (flag){
 			LOG_TRIVIAL(info)<<"end";
 			break;
-		}
-	
+		}	
+		client.send_msg_to_server(i, 0);
+		i++;
 	}
 
 
